8a9d446a9174b6b66e65522ddaffbcbce0e36d16
allen hsu <zzsh10011725@gmail.com>
2024-07-30 09:58:11 +0800

    age2.py

---
+++ age2.py
@@ -0,0 +1,19 @@
+driving = input('你有沒有開過車?')
+if driving != 'y' and driving!= 'n':
+	print('你打錯囉!')
+	raise SystemExit
+
+age = input('請輸入你的年齡:')
+age = int(age)
+
+if driving == 'y':
+    if age >=18:
+    	print('要小心開車窩!')
+    else:
+	    print('這樣會被警察抓走窩!')
+
+if driving == "n":
+	if age >= 18:
+		print('可以去考駕照囉!')
+	else:
+		print('要繼續保持!')
