965d1bfdd86fb36d60fdb51eb16ca95badb0f889
allen hsu <zzsh10011725@gmail.com>
2024-08-03 12:15:49 +0800

    age3.py

---
+++ age3.py
@@ -0,0 +1,18 @@
+driving = input('你有沒有開過車:')
+if driving !='y' and driving != 'n':
+	raise SystemExit
+
+age = input ('你幾歲:')
+age = int(age)
+
+if driving == 'y':
+	if age < 18:
+		print('壞壞!')
+	else:
+		print('小心開車!')
+
+if driving == 'n':
+	if age < 18:
+		print('繼續保持!')
+	else:
+		print('為甚麼不去考呢??')
